c44cea8164686626d49803e6b56c764ee0646cc1
Slava Osipov <skybright@mail.ru>
2024-02-14 22:18:29 +0300

    Initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("lifecycle");
+        context.close();
+    }
+}
